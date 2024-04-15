ddb8078bd16d656639bf489e3379bc00fa113457
David Arhin <davidarhin2005@gmail.com>
2024-04-15 09:13:18 +0000

    redux

---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    contacts: []
+  };
+  
+  const contactReducer = (state = initialState, action) => {
+    // Handle actions here
+    return state;
+  };
+  
+  export default contactReducer;
+  
